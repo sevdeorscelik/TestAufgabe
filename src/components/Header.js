2d71770992ae14972cf45b5c4e8e187d5e12247e
Sevde <sevdeorscelik@gmail.com>
2022-06-22 22:31:51 +0200

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from "react"
 const Header = () => {
     return (
         
-            <div className='Header position-relative d-flex justify-content-center align-items-center mb-5 '>
+            <div className='Header position-relative d-flex justify-content-center align-items-center mb-md-5 '>
 
-                <div className='position-absolute d-flex flex-column justify-content-center align-items-center user-container'>
+                <div className='position-absolute d-flex flex-sm-column flex-row justify-content-center align-items-center user-container'>
 
-                    <div className='position-relative '>
+                    <div className='position-relative me-sm-0 me-5'>
 
                         <div className=' overflow-hidden ps-2 pe-2 rounded-circle box-user'>
                             <i className="fas fa-user p-2 user-icon "></i>
@@ -17,7 +17,7 @@ const Header = () => {
 
                     </div>
 
-                    <p className='fs-4 fw-bold color-blue mb-0 user-name'>devel oper</p>
+                    <p className='fs-4 ms-sm-0 ms-5 fw-bold color-blue mb-0 user-name'>devel oper</p>
 
                 </div>
             </div>
