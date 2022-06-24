4117e8ab9d27d9ba8dfb9f2fbd6d081894d0aad8
Sevde <sevdeorscelik@gmail.com>
2022-06-24 02:10:28 +0200

    input fixed

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,53 +2,63 @@ import React from "react";
 
 import { useRef, useEffect, useState } from "react";
 
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, useFormik } from 'formik';
 
 import * as Yup from 'yup';
 
 
-
-
-
 const Popup = (props) => {
 
+    const SignupSchema = Yup.object().shape({
+        email: Yup.string().email('Bitte geben Sie gültige E-Mail-Adresse an! ').required('Bitte füllen Sie beide Felder aus!'),
+        emailConfirm: Yup.string().oneOf([Yup.ref('email')], 'Beide Felder müssen gleich sein!').required('Bitte füllen Sie beide Felder aus!')
+    });
+
+
+    const { errors, touched, values, handleSubmit, handleChange } = useFormik({
+        initialValues: {
+            email: '',
+            emailConfirm: ''
+        },
+        onSubmit: (values) => {
+
+            console.log('form submit oldu');
+
+            const newValues = JSON.stringify(values, null, 2);
+            const newEmail = JSON.parse(newValues).email;
+
+            props.setEmailOnUI(newEmail);
+
+            document.getElementById("aktuel-email").value = newEmail;
+
+            values.email = ""
+            values.emailConfirm = ""
+
+        },
+        validationSchema: SignupSchema
+
+    })
+    console.log(values);
 
 
 
     const closeBtn = useRef(null);
-
     const modal = useRef(null);
-
-    const input1 = useRef(null);
-
-    const input2 = useRef(null);
-
-    const [showModal, setShowModal] = useState(false);
-
-
-
-
     const formRef = useRef()
-
-
+    const input1 = useRef('');
+    const input2 = useRef();
+    const [showModal, setShowModal] = useState(false);
 
 
     // check if element contains class on mount 
 
     useEffect(() => {
-
         if (modal.current.classList.contains('show')) {
-
             setShowModal(true);
-
         } else {
-
             setShowModal(false);
-
         }
-
     }, []);
-
 
 
 
@@ -57,11 +67,9 @@ const Popup = (props) => {
     const handleClick = event => {
 
         if (event.currentTarget.classList.contains('show')) {
-
             setShowModal(true);
 
         } else {
-
             setShowModal(false);
 
         }
@@ -70,205 +78,82 @@ const Popup = (props) => {
 
 
 
-
-    if (showModal) {
-
-        console.log('acik');
-
-    } else {
-
-        console.log('kapali');
-
-        setTimeout(() => {
-
-            document.getElementById("email").value = "";
-
-            document.getElementById("emailConfirm").value = "";
-
-        }, 500);
-
+    if ((values.email !== values.emailConfirm) || (!values.email.includes('@') && !values.email.includes('.') && !values.emailConfirm.includes('@') && !values.emailConfirm.includes('.'))) {
+        if (!showModal ) {
+            values.email = ""
+            values.emailConfirm = ""
+        }
     }
-
-
-
-
-    const SignupSchema = Yup.object().shape({
-
-        email: Yup.string().email('Bitte geben Sie gültige E-Mail-Adresse an! ').required('Bitte füllen Sie beide Felder aus!'),
-
-        emailConfirm: Yup.string().oneOf([Yup.ref('email')], 'Beide Felder müssen gleich sein!').required('Bitte füllen Sie beide Felder aus!')
-
-    });
-
-
 
 
     return (
 
         <div ref={modal} onClick={handleClick} className="Popup modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-
             <div className="modal-dialog">
-
                 <div className="modal-content">
-
                     <div className="modal-header">
-
                         <h5 className="modal-title" id="exampleModalLabel">E-Mail-Adresse ändern</h5>
-
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-
                     </div>
 
                     <div className="modal-body">
-
                         <p>
-
                             Bitte gib deine Repeate E-Mail-Adresse zweimal an. Deine alte E-Mail-Adresse wird damit für das Portal ungültig. Bei Angeboten oder Fragen werden wir dich künftig über die Repeate E-Mail-Adresse kontaktieren. Um wieder alle Funktionen des Portals nutzen zu können, musst du deine Repeate E-Mail-Adresse bestätigen.
-
                         </p>
 
-                        <Formik innerRef={formRef}
+                        <Formik innerRef={formRef}>
 
-                            initialValues={{
+                            <Form onSubmit={handleSubmit}>
 
-                                email: '',
+                                <div className="mb-3">
 
-                                emailConfirm: ''
+                                    <Field onChange={handleChange} value={values.email} id="email" ref={input1} placeholder="Neue E-Mail-Adresse" className="form-control border-none" name="email" type="email" />
 
-                            }}
+                                    {errors.email && touched.email && showModal
+                                        ? (errors.email.includes('gültige')
+                                            ? (<div className="alert alert-primary mt-2 p-1 text-center"> {errors.email}</div>)
+                                            : (<div className="alert alert-danger mt-2 p-1 text-center"> {errors.email}</div>))
+                                        : null}
+                                </div>
 
+                                <div className="mb-3">
 
+                                    <Field onChange={handleChange} value={values.emailConfirm} id="emailConfirm" ref={input2} placeholder="Neue E-Mail-Adresse wiederholen" className="form-control border-none" name="emailConfirm" type="email" />
 
+                                    {errors.emailConfirm && touched.emailConfirm && showModal
 
-                            validationSchema={SignupSchema}
+                                        ? (errors.emailConfirm.includes('gleich'))
+                                            ? (<div className="alert alert-warning mt-2 p-1 text-center" >{errors.emailConfirm}</div>)
+                                            : (<div className="alert alert-danger mt-2 p-1 text-center" >{errors.emailConfirm}</div>)
+                                        : null}
 
+                                </div>
 
 
+                                <div className="d-flex justify-content-center mt-2">
 
-                            onSubmit={values => {
+                                    {
+                                        (values.email.length !== 0 && values.emailConfirm.length !== 0) && (!errors.email && !errors.emailConfirm)
 
-                                console.log('form submit oldu');
+                                            ? <button type="submit" ref={closeBtn} className='btn btn-submit text-white p-2 pe-5 ps-5 fw-bold bg-red w-75' data-bs-dismiss="modal" aria-label="Close">SPEICHERN</button>
 
+                                            : <button type="submit" ref={closeBtn} className='btn btn-submit text-white p-2 pe-5 ps-5 fw-bold bg-red w-75' >SPEICHERN</button>
 
+                                    }
 
+                                </div>
 
-                                const newValues = JSON.stringify(values, null, 2);
-
-                                const newEmail = JSON.parse(newValues).email;
-
-
-
-
-                                props.setEmailOnUI(newEmail);
-
-                                document.getElementById("aktuel-email").value = newEmail;
-
-
-
-
-                                values.email = ""
-
-                                values.emailConfirm = ""
-
-                            }}
-
-                        >
-
-
-
-
-                            {({ errors, touched, values, isSubmitting }) => (
-
-                                <Form>
-
-
-
-
-                                    {/* {showModal ? null : values.email = ""} 
-
-{showModal ? null : values.emailConfirm = ""} */}
-
-
-
-
-
-                                    <div className="mb-3">
-
-                                        <Field id="email" placeholder="Neue E-Mail-Adresse" className="form-control border-none" name="email" type="email" />
-
-                                        {errors.email && touched.email && showModal
-
-                                            ? (errors.email.includes('gültige')
-
-                                                ? (<div className="alert alert-primary mt-2 p-1 text-center"> {errors.email}</div>)
-
-                                                : (<div className="alert alert-danger mt-2 p-1 text-center"> {errors.email}</div>))
-
-                                            : null}
-
-
-
-
-                                    </div>
-
-                                    <div className="mb-3">
-
-                                        <Field id="emailConfirm" placeholder="Neue E-Mail-Adresse wiederholen" className="form-control border-none" name="emailConfirm" type="email" />
-
-                                        {errors.emailConfirm && touched.emailConfirm && showModal
-
-                                            ? (errors.emailConfirm.includes('gleich'))
-
-                                                ? (<div className="alert alert-warning mt-2 p-1 text-center" >{errors.emailConfirm}</div>)
-
-                                                : (<div className="alert alert-danger mt-2 p-1 text-center" >{errors.emailConfirm}</div>)
-
-                                            : null}
-
-                                    </div>
-
-
-
-
-                                    <div className="d-flex justify-content-center mt-2">
-
-
-
-
-                                        {
-
-                                            (values.email.length !== 0 && values.emailConfirm.length !== 0) && (!errors.email && !errors.emailConfirm)
-
-                                                ? <button type="submit" ref={closeBtn} className='btn btn-submit text-white p-2 pe-5 ps-5 fw-bold bg-red w-75' data-bs-dismiss="modal" aria-label="Close">SPEICHERN</button>
-
-                                                : <button type="submit" ref={closeBtn} className='btn btn-submit text-white p-2 pe-5 ps-5 fw-bold bg-red w-75' >SPEICHERN</button>
-
-                                        }
-
-                                    </div>
-
-                                </Form>
-
-                            )}
-
-
-
+                            </Form>
 
                         </Formik>
-
                     </div>
-
                 </div>
-
             </div>
-
         </div>
 
     )
 
 };
-
-
 
 
 export default Popup;
--- src/components/SuccessAlert.js
+++ src/components/SuccessAlert.js
@@ -8,10 +8,10 @@ const SuccessAlert = () => {
     return (
 
 
-        <div class="SuccessAlert alert alert-success pt-4 pb-3 text-center fs-5" role="alert">
+        <div className="SuccessAlert alert alert-success pt-4 pb-3 text-center fs-5" role="alert">
 
             <p> Ihre E-Mail-Adresse wurde erfolgreich geändert!</p>
-            <i class="fas fa-check-circle fs-1 mt-2"></i>
+            <i className="fas fa-check-circle fs-1 mt-2"></i>
 
         </div>
 
